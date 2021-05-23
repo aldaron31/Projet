3d78b2f2c9681cab3cc309c84417af6237a3f37b
Elève <Elve@UTILISA-GTPV8UR>
2021-05-23 18:49:39 +0200

    test

--- src/main/java/fr/insa/gonzalvo/projet/Treillis.java
+++ src/main/java/fr/insa/gonzalvo/projet/Treillis.java
@@ -5,13 +5,19 @@
  */
 package fr.insa.gonzalvo.projet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import fr.insa.gonzalvo.projet.Terrain;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author gonza
@@ -118,17 +124,13 @@ public class Treillis {
        }
        return res+"}";
    }
-   /*
+   
    public static void NoeudPoint(){
     Noeud_Simple n1= new Noeud_Simple(1,2);
     Noeud_Simple n2= new Noeud_Simple(3,2);
     Noeud_Simple n3= new Noeud_Simple(4,2);   
     Noeud_Simple n4= new Noeud_Simple(5,3);
     Barre b1= Barre(n1,n2);
-   }
-   */
-   public static void main(String[] args){
-      
    }
    
    public static Treillis TreillisTest() {
@@ -146,18 +148,129 @@ public class Treillis {
             b.dessine(context);
         }
     }
-
-    /**
-     * @return the TerrainT
-     */
     public Terrain getTerrainT() {
         return TerrainT;
     }
-
-    /**
-     * @param TerrainT the TerrainT to set
-     */
+    
     public void setTerrainT(Terrain TerrainT) {
         this.TerrainT = TerrainT;
+    }
+    
+    public static Color parseColor(String sr, String sg, String sb) {
+       double rouge = Double.parseDouble(sr);
+       double vert = Double.parseDouble(sg);
+       double bleu = Double.parseDouble(sb);
+       return Color.color(rouge, vert, bleu);
+    }
+    
+    public void save(Writer w, Numeroteur<Treillis> num, Numeroteur<Terrain> numT, Numeroteur<Triangle_Terrain> numTT, Numeroteur<Segment_Terrain> numS, Numeroteur<Point> numP) throws IOException {
+        if (!num.objExist(this)) {
+            int id = num.creeID(this);
+            this.getTerrainT().save(w, numT, numTT, numS, numP);
+            w.append("Treillis;" + id +";" + numT.getID(this.getTerrainT())+ "\n");
+        }
+    }
+    
+    public void sauvegarde(File fout) throws IOException {
+        Numeroteur<Treillis> num = new Numeroteur<Treillis>();
+        Numeroteur<Terrain> numT = new Numeroteur<Terrain>();
+        Numeroteur<Triangle_Terrain> numTT = new Numeroteur<Triangle_Terrain>();
+        Numeroteur<Segment_Terrain> numS = new Numeroteur<Segment_Terrain>();
+        Numeroteur<Point> numP = new Numeroteur<Point>();
+        try (BufferedWriter bout = new BufferedWriter(new FileWriter(fout))) {
+                this.save(bout, num, numT, numTT, numS, numP);
+        }
+    }
+    
+    public static void testSauvegarde() {
+        
+        Treillis TreillisTest = new Treillis(Terrain.TerrainTest());
+        try {
+            TreillisTest.sauvegarde(new File("TreillisTest.txt"));
+        } catch (IOException ex) {
+            throw new Error("Problème :" + ex.getMessage()); 
+        }
+    }
+    
+    public static Treillis lecture(File fin) throws IOException {
+        Numeroteur<Treillis> num = new Numeroteur<Treillis>();
+        Numeroteur<Terrain> numT = new Numeroteur<Terrain>();
+        Numeroteur<Triangle_Terrain> numTT = new Numeroteur<Triangle_Terrain>();
+        Numeroteur<Segment_Terrain> numS = new Numeroteur<Segment_Terrain>();
+        Numeroteur<Point> numP = new Numeroteur<Point>();
+        Treillis derniere = new Treillis();
+        try (BufferedReader bin = new BufferedReader(new FileReader(fin))) {
+            String line;
+            while ((line = bin.readLine()) != null && line.length() != 0) {
+                String[] bouts = line.split(";");
+                if (bouts[0].equals("Point")) {
+                    int id = Integer.parseInt(bouts[1]);
+                    double px = Double.parseDouble(bouts[2]);
+                    double py = Double.parseDouble(bouts[3]);
+                    Color col = Treillis.parseColor(bouts[4], bouts[5], bouts[6]);
+                    Point np = new Point(px, py, col);
+                    numP.associe(id, np);
+                } else if (bouts[0].equals("SegmentT")) {
+                    int id = Integer.parseInt(bouts[1]);
+                    int idP1 = Integer.parseInt(bouts[2]);
+                    int idP2 = Integer.parseInt(bouts[3]);
+                    Color col = Treillis.parseColor(bouts[4], bouts[5], bouts[6]);
+                    Point p1 = (Point) numP.getObj(idP1);
+                    Point p2 = (Point) numP.getObj(idP2);
+                    Segment_Terrain ns = new Segment_Terrain(p1, p2, col);
+                    numS.associe(id, ns);
+                } else if (bouts[0].equals("TriangleT")) {
+                    int id = Integer.parseInt(bouts[1]);
+                    int idP1 = Integer.parseInt(bouts[2]);
+                    int idP2 = Integer.parseInt(bouts[3]);
+                    int idP3 = Integer.parseInt(bouts[4]);
+                    int idS1 = Integer.parseInt(bouts[5]);
+                    int idS2 = Integer.parseInt(bouts[6]);
+                    int idS3 = Integer.parseInt(bouts[7]);
+                    Color col = Treillis.parseColor(bouts[8], bouts[9], bouts[10]);
+                    Point p1 = (Point) numP.getObj(idP1);
+                    Point p2 = (Point) numP.getObj(idP2);
+                    Point p3 = (Point) numP.getObj(idP3);
+                    Segment_Terrain s1 = (Segment_Terrain) numS.getObj(idS1);
+                    Segment_Terrain s2 = (Segment_Terrain) numS.getObj(idS2);
+                    Segment_Terrain s3 = (Segment_Terrain) numS.getObj(idS3);
+                    Triangle_Terrain ntt = new Triangle_Terrain(p1, p2, p3, s1, s2, s3, col);
+                    numTT.associe(id, ntt);
+                } else if (bouts[0].equals("Terrain")) {
+                    int id = Integer.parseInt(bouts[1]);
+                    Terrain nt = new Terrain();
+                    numT.associe(id, nt);
+                    for (int i = 2; i < bouts.length; i++) {
+                        int idSous = Integer.parseInt(bouts[i]);
+                        Triangle_Terrain TT = numTT.getObj(idSous);
+                        nt.getEnsembleTT().add(TT);
+                    }
+                    derniere.setTerrainT(nt);
+                } else if (bouts[0].equals("Treillis")) {
+                    int id = Integer.parseInt(bouts[1]);
+                    Treillis ntr = new Treillis();
+                    num.associe(id, ntr);
+                    int idTer = Integer.parseInt(bouts[2]);
+                    Terrain Ter = (Terrain) numT.getObj(idTer);
+                    ntr.setTerrainT(Ter);
+                }
+            }
+        }
+        
+        return derniere;
+    }
+    
+    public static void testLecture() {
+        try {
+            Treillis lue = Treillis.lecture(new File("TreillisTest.txt"));
+            System.out.println("Treillis lue : " + lue);
+        } catch (IOException ex) {
+            throw new Error(ex);
+        }
+    }
+    
+    public static void main(String[] args) {
+        //testSauvegarde();
+        testLecture();
     }
 }
